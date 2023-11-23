6836a45f64c615d19ccace04ddeeadcaa568c3ff
Tryfonov Denys <54479637+TrifonovDenys@users.noreply.github.com>
2023-11-23 22:35:51 +0200

    app

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 import express from "express";
 import logger from "morgan"
 import cors from "cors"
+import 'dotenv/config'
+console.log(process.env)
 
+// const usersRouter = require("./routes/users");
+// const contactsRouter = require("./routes/api/contacts");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+// app.use("/users", usersRouter);
+// app.use("/api/contacts", contactsRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message });
+});
+
+export default app
